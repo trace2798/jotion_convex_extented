67d6dcbed91a3daf7f2171b1dd490140bffe478c
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-11-29 13:40:02 +0530

    fixed issue with auth

---
+++ convex/auth.config.js
@@ -0,0 +1,10 @@
+export default {
+    providers: [
+      {
+        domain: "https://well-zebra-47.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ],
+  };
+  //applicationID is the name of the JWT template which we created
+  
